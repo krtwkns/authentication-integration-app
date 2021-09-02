558067e636de72fadb9bcbb5013b5bf8f4bc1575
Kretawiweka Nuraga Sani <kretawiweka.sani@shopee.com>
2021-09-03 00:04:30 +0700

    feat: create login integrate with API

--- src/containers/Login/index.jsx
+++ src/containers/Login/index.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import { Button, Form, Input } from 'antd';
+import fetchData from '../../utils/fetchData';
 import MobileViewWrapper from '../../components/MobileViewWrapper';
 
 const Login = () => {
+  const onFinish = async value => {
+    const res = await fetchData({
+      url: `${process.env.REACT_APP_BASE_API}/login`,
+      method: 'POST',
+      data: {
+        username: value.username,
+        password: value.password
+      }
+    });
+    console.log(res);
+  };
   return (
     <MobileViewWrapper>
       <h1>Login</h1>
-      <Form>
+      <Form layout="vertical" onFinish={onFinish}>
         <Form.Item
           label="Username"
           name="username"
